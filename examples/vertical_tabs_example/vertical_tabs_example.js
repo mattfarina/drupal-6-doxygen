94bdc6414f9e128410bde845c3af72d43e9fd054
Matt Farina <matt@mattfarina.com>
2010-09-14 08:18:32 -0400

    Added the examples modules (http://drupal.org/project/examples)

---
+++ examples/vertical_tabs_example/vertical_tabs_example.js
@@ -0,0 +1,18 @@
+// $Id: vertical_tabs_example.js,v 1.1.2.2 2010/02/10 21:46:26 davereid Exp $
+
+// The following line will prevent a JavaScript error if this file is included
+// and vertical tabs are disabled.
+Drupal.verticalTabs = Drupal.verticalTabs || {};
+
+Drupal.verticalTabs.vertical_tabs_example = function() {
+  if ($('#edit-vertical-tabs-example-enabled').attr('checked')) {
+    // We show here the container with the custom settings form.
+    $('#edit-vertical-tabs-example-custom-setting-wrapper').parent().show('fast')
+    return Drupal.checkPlain($('#edit-vertical-tabs-example-custom-setting').val());
+  }
+  else {
+    // We hide here the container with the custom settings form.
+    $('#edit-vertical-tabs-example-custom-setting-wrapper').parent().hide('fast')
+    return Drupal.t('Using default');
+  }
+}
